f7c72f280dbccaf3561ab7b6bc8b3756ec245ca0
jfchilito7 <jfchilito7@gmail.com>
2024-10-25 14:15:39 -0500

    Añadir archivos de configuración y estilos CSS de Tailwind
    Esta confirmación añade los archivos de configuración y estilos necesarios para Tailwind CSS en tu proyecto.

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import { useState } from 'react'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className="App">
+      <div className='text-blue-700 font-bold'>Hello world</div>
+    </div>
+  )
+}
+
+export default App
